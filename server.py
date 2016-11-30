a877f3b21b18c2850d93cad5865ade74d281f66f
Danny Peck <danny.peck@inin.com>
2016-11-30 11:31:02 -0500

    added explanatory comments

--- server.py
+++ server.py
@@ -1,29 +1,39 @@
+# components of the bottle library must be imported by name not with *
 from bottle import route, run, template
 from bottle import get, post, request
 
+# index route
 @route ('/')
 def index ():
     return template("index.html")
 
+# route explanation
 @route ('/route')
 def routes ():
     return template("route.html")
 
+# request explanation
 @route ('/requests')
 def requests ():
     return template("requests.html")
 
+# retrieves login.html
 @get ('/login')
 def examples ():
     return template("login.html")
 
+# performs login request
 @post ('/login')
 def do_login():
-  userName = request.forms.get('userName')
-  password = request.forms.get('password')
-  if password == "bottle":
+    # reads values from the input elements named userName and password
+    userName = request.forms.get('userName')
+    password = request.forms.get('password')
+
+    # verifies credentials
+    if password == "bottle":
     return "<h2>valid login, " + userName + ".</h2>"
-  else:
+    else:
     return "<h2>invalid login!</h2>"
 
+# starts the server running on localhost:8080
 run (host = "localhost", port = 8080, debug = True)
